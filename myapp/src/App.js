1b8b3d8ae76416d7ff0fdfd4ba84b3b766fb383b
Monojit <monojithbhattacharya@gmail.com>
2025-03-09 18:27:04 +0530

    added css module
    added taiwind css module

---
+++ myapp/src/App.js
@@ -0,0 +1,11 @@
+import React from "react";
+import "../src/index.css";
+
+
+function App() {
+    return(
+        <h1 className="text-3xl">Hello world!</h1>
+    )
+}
+
+export default App;
--- myapp/src/index.js
+++ myapp/src/index.js
@@ -1,9 +1,8 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
+import App from "../src/App"
+import "../src/index.css";
 
-function App() {
-  return <h2>my app</h2>;
-}
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
