160e7185b65819496da6eec5f2a0ad7cd9865bb6
Monojit <monojithbhattacharya@gmail.com>
2025-04-06 12:31:47 +0530

    Reapply "DEv: created build"
    This reverts commit 68da9372e523db71fc5c88d4f6c04bc910718cac.

--- backend/server.js
+++ backend/server.js
@@ -10,7 +10,16 @@ app.use(express.json());
 app.use("/api/", authRoutes);
 
 connectDB().then(() => {
-        console.log("MongoDB connected");
-        app.listen(5000, () => console.log("Server running on port 5000"));
-    })
+    console.log("MongoDB connected");
+    app.listen(5000, () => console.log("Server running on port 5000"));
+})
     .catch(err => console.log(err));
+
+
+const path = require('path');
+
+app.use(express.static(path.join(__dirname, '../frontend/build')));
+
+app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname, '../frontend/build', 'index.html'));
+});
